08d5b63abc91c645b260613d4a96f5819dc8d00a
Wastedplatypus <jojjewi@gmail.com>
2018-09-06 14:38:37 +0200

    commit

---
+++ 2018-09-06/2.1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	 int a = 4, b;
+	 b = 13-a;
+	 2*b = a;
+	 12 = a;
+	 return 0;
+}
---
+++ 2018-09-06/uppgifter.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	 
+	 cout << "Johan" << endl;
+	 cout << "" << endl;
+	 cout << "varbergsvägen 345A" << endl;
+	
+	
+	
+	 cout << " CCCCC" << endl;
+	 cout << "C            +     +" << endl;
+	 cout << "C          +++++ +++++" << endl;
+	 cout << "C            +     +" << endl;
+	 cout << " CCCCC" << endl;
+	 
+	 
+	 
+	 cout << "  \"\"\"   " << endl;
+	 
+	 cout << "\\\\    // " << endl;
+	 cout << "  o  o   " << endl;
+	 cout << "   U     " << endl;
+	 
+	 cout << "  ---    " << endl;
+	
+	
+	
+	 return 0;
+}
