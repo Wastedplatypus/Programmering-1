99d10fb0d049cc53f9a5e58c3ae87d0b8f77ef08
Wastedplatypus <jojjewi@gmail.com>
2018-11-29 14:54:58 +0100

    commit

---
+++ 2018-11-29/Exempel 8.3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	  int hastighet[8];
+	  
+	  for (int n=0; n<8; n++) {
+		  hastighet[n] = 5*(n+1); }
+		  
+	 for (int n=0; n<8; n++) {
+		 cout << hastighet[n] << ' '; }
+		 
+		 cout << endl;
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
