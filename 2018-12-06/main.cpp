755cdbcf1a48ebf42423c70e7a38d4d3abdeae0a
Wastedplatypus <jojjewi@gmail.com>
2018-12-06 15:02:37 +0100

    commit

---
+++ 2018-12-06/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+// continue och break, 8.4, sidan 119
+int main()
+{
+	 
+	 for (int i=1; i<100; i++)
+	 {
+		  for (int j=2; j<100; j++)
+		  
+	{
+	      if (i!=j)
+	      
+    {
+		if ( (i % j ) != 0 )
+		
+	{
+	
+	cout << i << endl;
+    }
+    }
+    
+    
+    }
+    
+    }
+	
+	
+	
+	 return 0;
+}
