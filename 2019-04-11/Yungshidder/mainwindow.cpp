ea0fd3cf51d059fed822b4750102e6f2cecac027
Wastedplatypus <jojjewi@gmail.com>
2019-04-11 14:53:36 +0200

    commit

---
+++ 2019-04-11/Yungshidder/mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(peehole()));
+}
+
+// private slots
+void MainWindow::peehole()
+{
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
